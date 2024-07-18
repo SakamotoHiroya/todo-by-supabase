3349dc15dad5bce5b2749ebd938f1af77a314804
SakamotoHiroya <ayorih16@icloud.com>
2024-07-18 16:32:41 +0900

    create header

---
+++ components/Logo.tsx
@@ -0,0 +1,32 @@
+import { SquareCheckBig } from "@yamada-ui/lucide"
+import styles from "./styles/logo.module.css"
+import { M_PLUS_Rounded_1c } from "next/font/google";
+
+const logoFont = M_PLUS_Rounded_1c({ subsets: ["latin"], weight: "400" });
+
+type Size = "small" | "medium" | "large" | "extra-large"
+const sizeStyles = {
+    "small": styles.small,
+    "medium": styles.medium,
+    "large": styles.large,
+    "extra-large": styles.extraLarge,
+}
+
+type Props = {
+    size: Size,
+}
+
+const Logo = ({size} : Props) => {
+
+    console.log();
+    return(
+        <h1 className={`${styles.logo} ${sizeStyles[size]}`}>
+            
+            <span className={`${styles.text} ${logoFont.className}`}><span className={styles.firstLetter}>T</span>od</span>
+            <SquareCheckBig className={styles.icon}/>
+            <span className={`${styles.text} ${logoFont.className}`}>List</span>
+        </h1>
+    )
+}
+
+export default Logo;
