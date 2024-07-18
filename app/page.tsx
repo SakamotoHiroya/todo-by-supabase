5eebbc7b895d1dffcc9e0502620f285de5a8db2d
SakamotoHiroya <ayorih16@icloud.com>
2024-07-18 13:54:24 +0900

    make index page to protected

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,18 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
 export default async function Index() {
+
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  //If not logged in, redirect to /login
+  if (!user) {
+    return redirect("/login");
+  }
 
   return (
     <div>
